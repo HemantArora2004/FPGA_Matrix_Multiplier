de945624cb2e160c0c22d138aee670c7afdbd76a
HemantArora2004 <HemantArora2004@gmail.com>
2024-05-15 22:00:47 -0400

    Added TCL files

---
+++ TCL/bulid.tcl
@@ -0,0 +1,31 @@
+# Define the path to your existing project
+set project_path "C:/Users/Hemant/Documents/Programming/XilinxFPGA/Matrix_Multiplier/Matrix_Multiplier.xpr"
+
+# Define a list of Verilog files to synthesize
+set verilog_files {
+    "C:/Users/Hemant/Documents/Programming/XilinxFPGA/Matrix_Multiplier/Matrix_Multiplier.GitHub/Source/Matrix_Multiplier.sv"
+}
+
+# Define the path to your constraints file
+set constraints_file "C:/Users/Hemant/Documents/Programming/XilinxFPGA/Matrix_Multiplier/Matrix_Multiplier.GitHub/Constraint/Basys-3-Master.xdc"
+
+# Open the existing project
+open_project $project_path
+
+# Add specified Verilog files to the project
+foreach file $verilog_files {
+    add_files $file
+}
+
+# Add constraints file to the project
+add_files $constraints_file
+
+# Reset synthesis run if needed
+reset_run synth_1
+
+# Run synthesis
+launch_runs synth_1 -jobs 12
+
+# Wait for synthesis to complete
+wait_on_run synth_1
+
