4853136889fa486f295d3621da203b31c05cff5b
HemantArora2004 <HemantArora2004@gmail.com>
2024-08-25 12:01:21 -0400

    updated fp32_mult_tb

--- Simulation/fp32_multiplier_tb/fp32_multiplier_tb.sv
+++ Simulation/fp32_multiplier_tb/fp32_multiplier_tb.sv
@@ -20,20 +20,30 @@ logic [31:0] result;
 logic done;
 
 int operand_index;
-int i, j;
+int product_index;
 
 initial begin
         $readmemh("memory_operands.mem", memory_operands);
         $readmemh("memory_products.mem", memory_products);
         start <= 1'b0;
         operand_index <= 0;
+        product_index <= 0;
         rst_n <= 0; #10;
         rst_n <= 1; #10;
         
-        for(i = 0; i < OPERANDS; i = i + 2) begin
-            start <= 1'b1;
-            a <= memory_operands [i];
-            b <= memory_operands [i + 1]; #10;            
+        while(operand_index < OPERANDS || product_index < PRODUCTS) begin
+            if(operand_index < OPERANDS) begin
+                start <= 1'b1;
+                a <= memory_operands[operand_index];
+                b <= memory_operands[operand_index + 1];
+                operand_index = operand_index + 2;
+            end
+            
+            if(done) begin
+                product_index = product_index + 1;
+                if(result != memory_products[product_index]) error <= 1'b1;
+                else  error <= 1'b0;
+            end
         end
         
         start <= 1'b0;
